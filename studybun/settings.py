107e3ce0baddd497a01e9ce77b504cfd5ccbd63a
S.Mahesh <105922665+Seelammahesh@users.noreply.github.com>
2025-11-04 00:33:12 +0530

    second

--- studybun/settings.py
+++ studybun/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'your-very-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://bunny-dusky-seven.vercel.app/']
 
 # Application definition
 INSTALLED_APPS = [
