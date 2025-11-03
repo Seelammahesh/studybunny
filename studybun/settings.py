d14a05e802a0df8b0f39aea3ac4a549930d62b51
S.Mahesh <105922665+Seelammahesh@users.noreply.github.com>
2025-11-04 00:51:25 +0530

    fourth

--- studybun/settings.py
+++ studybun/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your-very-secret-key'
+SECRET_KEY = 'yourverysecre-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
